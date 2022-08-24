3c355908250a886d92a4fe436af8cbe7fc274e67
mubinfshaikh <imubinshaikh@gmail.com>
2022-08-24 21:24:33 +0530

    created ami shell scripts

---
+++ scripts/cloud/aws/ami/restore-ami-from-s3.sh
@@ -0,0 +1,6 @@
+/usr/local/bin/aws ec2 create-restore-image-task \
+    --object-key ami-00a3448b2e67fade8.bin \
+    --bucket cargofl-ami \
+    --name "CargoFL AMI"	
+
+	
---
+++ scripts/cloud/aws/ami/store-ami-into-s3.sh
@@ -0,0 +1,5 @@
+/usr/local/bin/aws ec2 create-store-image-task \
+    --image-id ami-00a3448b2e67fade8 \
+    --bucket cargofl-ami
+	
+	

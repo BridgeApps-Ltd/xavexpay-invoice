a131c387bd3c0df6b38da6989df9d7a3b053a6a2
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2022-08-25 21:06:11 +0530

    Create stop-ec2-instance.sh

---
+++ scripts/cloud/awscli/stop-ec2-instance.sh
@@ -0,0 +1,5 @@
+#----------------------Note this instance id is for example-----------------------#
+#---Note you must have awscli installed and proper AMI policy to Stop instance---#
+
+echo "Stopping ec2 Instance"
+aws ec2 stop-instances --region ap-south-1 --instance-ids i-0123456789abcdef

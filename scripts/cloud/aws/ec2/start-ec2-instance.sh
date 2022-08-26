cb67c901ce8289863944e5e77d8d1fbba9977bfa
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2022-08-26 11:04:32 +0530

    Update start-ec2-instance.sh

--- scripts/cloud/aws/ec2/start-ec2-instance.sh
+++ scripts/cloud/aws/ec2/start-ec2-instance.sh
@@ -1,5 +1,5 @@
 #----------------------Note this instance id is for example-----------------------#
 #---Note you must have awscli installed and proper AMI policy to Start instance---#
-
+instance_id="i-0123456789abcdef"
 echo "Starting ec2 Instance"
-aws ec2 start-instances --region ap-south-1 --instance-ids i-0123456789abcdef
+aws ec2 start-instances --region ap-south-1 --instance-ids ${instance_id}

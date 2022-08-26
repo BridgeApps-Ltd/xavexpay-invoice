03af0b7c0ed48ef31253fe0eded7c2cea737cd1c
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2022-08-26 16:36:03 +0530

    Update store-ami-into-s3.sh

--- scripts/cloud/aws/ami/store-ami-into-s3.sh
+++ scripts/cloud/aws/ami/store-ami-into-s3.sh
@@ -1,6 +1,8 @@
 echo "... Initiating storing ami into S3 Bucket" 
-/usr/local/bin/aws ec2 create-store-image-task \
-    --image-id ami-00a3448b2e67fade8 \
-    --bucket cargofl-ami
+
+ami_id="ami-00a3448b2e67fade8"
+s3_bucket_name="cargofl-ami"
+
+/usr/local/bin/aws ec2 create-store-image-task --image-id ${ami_id} --bucket ${s3_bucket_name}
 echo "... Completed storing ami"
 	

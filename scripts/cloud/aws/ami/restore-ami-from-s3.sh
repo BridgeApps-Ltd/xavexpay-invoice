94dca46150bb7520c32914aae087c14d8578a819
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2022-08-26 16:36:58 +0530

    Update restore-ami-from-s3.sh

--- scripts/cloud/aws/ami/restore-ami-from-s3.sh
+++ scripts/cloud/aws/ami/restore-ami-from-s3.sh
@@ -1,7 +1,8 @@
 echo "...  restoring ami from S3 Bucket" 
-/usr/local/bin/aws ec2 create-restore-image-task \
-    --object-key ami-00a3448b2e67fade8.bin \
-    --bucket cargofl-ami \
-    --name "CargoFL AMI"	
+s3_bucket_name="cargofl-ami"
+s3_object_name="ami-00a3448b2e67fade8.bin"
+ami_name="CargoFL AMI"
+
+/usr/local/bin/aws ec2 create-restore-image-task --object-key ${s3_object_name} --bucket ${s3_bucket_name} --name ${ami_name}	
 echo "... Completed" 
 	

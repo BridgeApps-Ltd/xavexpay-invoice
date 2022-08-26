9115ddecd9323738f1bf8017b695b72b7e297b69
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2022-08-26 11:39:33 +0530

    Update stop-ec2-instance.sh

--- scripts/cloud/aws/ec2/stop-ec2-instance.sh
+++ scripts/cloud/aws/ec2/stop-ec2-instance.sh
@@ -1,6 +1,10 @@
 #----------------------Note this instance id is for example-----------------------#
 #---Note you must have awscli installed and proper AMI policy to Stop instance---#
+
+green=`tput setaf 2`
+white=`tput setaf 7`
 instance_id="i-0123456789abcdef"
 instance_name=$(aws ec2 describe-tags --filters Name=resource-id,Values=${instance_id} Name=key,Values=Name --query Tags[].Value --output text)
-echo "Stopping ec2 Instance ${instance_name}"
+
+echo "Stopping ec2 Instance => ${green}${instance_name}${white}"
 aws ec2 stop-instances --region ap-south-1 --instance-ids ${instance_id}

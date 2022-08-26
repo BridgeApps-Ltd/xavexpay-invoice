7854bb630a2c0274d16ba707e570f2461b2b1d43
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2022-08-26 11:29:19 +0530

    Update start-ec2-instance.sh

--- scripts/cloud/aws/ec2/start-ec2-instance.sh
+++ scripts/cloud/aws/ec2/start-ec2-instance.sh
@@ -1,5 +1,6 @@
 #----------------------Note this instance id is for example-----------------------#
 #---Note you must have awscli installed and proper AMI policy to Start instance---#
 instance_id="i-0123456789abcdef"
-echo "Starting ec2 Instance"
+instance_name=$(aws ec2 describe-tags --filters Name=resource-id,Values=${instance_id} Name=key,Values=Name --query Tags[].Value --output text)
+echo "Starting ec2 Instance => ${instance_name}"
 aws ec2 start-instances --region ap-south-1 --instance-ids ${instance_id}

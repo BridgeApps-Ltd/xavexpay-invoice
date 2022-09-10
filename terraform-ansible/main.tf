acaeded7d32acffee0fd1ed8117b402fe23a7e57
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2022-09-10 13:07:33 +0530

    Update main.tf

--- terraform-ansible/main.tf
+++ terraform-ansible/main.tf
@@ -1,11 +1,13 @@
 module "trent-test" {
         source = "./modules/templates/test/trent-test"
 	gitpass = var.gitpass
+	keyPath = var.keyPath
     }
 
 /*
 module "trent-prod" {
         source = "./modules/templates/production/trent-prod"
         gitpass = var.gitpass
+	keyPath = var.keyPath
     }
 */

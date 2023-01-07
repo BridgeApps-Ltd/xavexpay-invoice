bc58cdc9c1fc417b4baedb7d8393d9d1e8dc7665
Nandagopal M <nandu@Nandagopals-MacBook-Pro.local>
2023-01-07 22:34:29 +0530

    refactored the app

---
+++ aws-inspector/app.py
@@ -0,0 +1,166 @@
+from re import I
+import socket
+import boto3
+import pandas as pd
+import json
+import os
+import argparse
+from flask import Flask, render_template, request
+import warnings
+
+warnings.filterwarnings("ignore")
+from aws_inspector import AWSInspector
+
+args = {}
+
+absolute_path = os.path.dirname(os.path.abspath(__file__))
+
+aws_inspector = None
+
+all_services_data = {}
+s3_bucket_data = []
+
+app = Flask(__name__)
+
+def merge_data(data_list):
+    all_dfs = []
+    for json_data in data_list:
+        all_dfs.append(pd.DataFrame(json_data))
+    df = pd.concat(all_dfs)
+    all_services_data = json.loads(df.to_json(orient='records'))
+    return all_services_data
+
+def process_instance_info(instance_id):
+    if (instance_id.startswith('ec2-')):
+        # EC2 Instance
+        instance_id = instance_id.removeprefix('ec2-')
+        instance_info = aws_inspector.ec2.describe_instances(InstanceIds=[instance_id])
+    elif (instance_id.startswith('rds-')):
+        instance_id = instance_id.removeprefix('rds-')
+        instance_info = aws_inspector.rds.describe_db_instances(DBInstanceIdentifier=instance_id)
+    else:
+        instance_info = "No information is retrieved for this AWS Service type" 
+
+    return instance_info
+
+
+def merge_all_data(ec2_json, rds_json, s3_json):
+    ec2_df = pd.DataFrame(ec2_json)
+    s3_df = pd.DataFrame(s3_json)
+    rds_df = pd.DataFrame(rds_json)
+    df = pd.concat([ec2_df, s3_df, rds_df])
+    all_services_data = json.loads(df.to_json(orient='records'))
+    return all_services_data
+
+def get_all_aws_profiles():
+    all_aws_profiles = boto3.session.Session().available_profiles
+    for profile in all_aws_profiles:
+        print("... Available Profiles : " + profile)
+    return all_aws_profiles
+
+def set_aws_profile(new_aws_profile):
+    #os.environ["AWS_DEFAULT_PROFILE"] = new_aws_profile
+    global aws_inspector
+    aws_inspector = AWSInspector(profile_name=new_aws_profile)
+    print(f'Current Session set as = {aws_inspector.current_profle}')
+
+    return "Successfully set profile to : " + new_aws_profile
+
+def get_current_aws_profile():
+    return boto3.session.Session().profile_name
+
+@app.route('/', methods=['GET'])
+def page_home():
+   return render_template('index.html')
+
+@app.route('/tree', methods=['GET'])
+def page_home_tree():
+   return render_template('indextree.html')
+
+
+@app.route('/api/v1/aws/services/all', methods=['GET'])
+def api_all_services_data():
+    vpc_json, ec2_json, rds_json, s3_json = aws_inspector.describe()
+    all_services_data = merge_data([ec2_json, rds_json, s3_json])
+    return all_services_data
+
+@app.route('/api/v1/aws/services/tree/all', methods=['GET'])
+def api_all_services_tree_data():
+    # resetAll()
+    vpc_json, ec2_json, rds_json, s3_json = aws_inspector.describe_for_tree()
+    all_services_data = merge_data([vpc_json, ec2_json, rds_json, s3_json])
+    return all_services_data
+
+
+@app.route('/api/v1/aws/services/tree/<instance_id>', methods=['GET'])
+def api_instance_details(instance_id):
+    instance_info = process_instance_info(instance_id)
+    return instance_info
+
+@app.route('/api/v1/aws/profile/<profile_name>', methods=['POST'])
+def api_set_new_aws_profile(profile_name):
+    status = set_aws_profile(profile_name)
+    return status
+
+@app.route('/api/v1/aws/profile/<profile_name>', methods=['GET'])
+def api_list_aws_profiles(profile_name):
+    # all - is the only parameter processed for now
+    if (profile_name == 'all'):
+        all_aws_profiles = get_all_aws_profiles()
+        return all_aws_profiles
+    else:
+        return 'Error: no profile mentioned'
+
+@app.route('/api/v1/aws/profile', methods=['GET'])
+def api_get_current_aws_profile():
+    return get_current_aws_profile()
+
+
+def parse_inputs():
+    parser = argparse.ArgumentParser("aws-inspector")
+    parser.add_argument('-o','--output', help="Output. Options - web | file. Default file content format is JSON", type=str, default='web', required=True)
+    parser.add_argument('-op','--outputpath', help="Output Path to store output",   type=str, default='.', required=False)
+    parser.add_argument('-p','--port', help="port to run service",   type=int, default=5000, required=False)
+    args = vars(parser.parse_args())
+    return args
+
+def run_flask(port):
+    app.debug = True
+    app.run(port=port)
+
+def write_to_file(data, outputpath):
+
+    # Decide on outputpath, specific to OS
+    if (outputpath == '.'):
+        filepath = absolute_path + '/output'
+    else:
+        filepath = outputpath
+
+    print("... Creating file : " + filepath + '/merged.json' )
+
+    # Print out a Merged File 
+    with open(filepath + '/merged.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    # # Print out a Merged File for S3 buckets
+    # with open(filepath + '/buckets.json', 'w') as outfile:
+    #     json.dump(s3_bucket_data, outfile)
+
+def main():
+    args = parse_inputs()
+    
+    all_aws_profiles = get_all_aws_profiles()
+    set_aws_profile(all_aws_profiles[0])
+
+    if args["output"] == "web":
+        run_flask(args["port"])
+    else:
+        _, ec2_json, rds_json, s3_json = aws_inspector.describe()
+        all_services_data = merge_data([ec2_json, rds_json, s3_json])
+        write_to_file(all_services_data, args["outputpath"])
+        
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ aws-inspector/aws_inspector.py
@@ -0,0 +1,315 @@
+import pandas as pd
+import json
+import boto3
+import socket
+
+class AWSInspector:
+
+
+    def __init__(self, profile_name):
+
+        self.profile_name = profile_name
+        boto3.setup_default_session(profile_name=self.profile_name)
+        # Initialize with new profile
+        self.ec2 = boto3.client('ec2')
+        self.rds = boto3.client('rds')
+        self.s3 = boto3.client('s3')
+        
+    @property
+    def current_profle(self):
+        return boto3.session.Session().profile_name
+
+
+    def describe_vpcs(self):
+        vpc_json = []
+        vpcs = self.ec2.describe_vpcs()
+        vpcs = vpcs['Vpcs']
+        for data in vpcs:
+            vpcId = data['VpcId']
+            tags = data['Tags']
+            for tag in tags:
+                key = tag['Key']
+                value = tag['Value']
+                vpc_json.append({
+                        "VPCID":"{}".format(vpcId),
+                        "Tag":f"VPC:{key} | Customer:{value}", 
+                        "Service":"VPC"
+                        })
+        return vpc_json
+
+    # Describe VPCs first
+    def describe_vpcs_as_tree(self):
+        vpc_json = []
+        vpcs = self.ec2.describe_vpcs()
+        vpcs = vpcs['Vpcs']
+
+        for data in vpcs:
+            vpcId = data['VpcId']
+            vpcDataset = {
+                "id" : vpcId,
+                "parent": "#"
+            }
+            tags = data['Tags']
+            vpc_desc = "VPC"
+            for tag in tags:
+                key = tag['Key']
+                value = tag['Value']
+                vpc_tag_desc =  " | " + tag['Value'] 
+                vpc_desc = vpc_desc + vpc_tag_desc
+            vpcDataset["text"] = vpc_desc
+            vpc_json.append(vpcDataset)
+
+            # Sublist EC2
+            ec2ParentDs = {
+                "id" : "ec2-" + vpcId,
+                "parent": vpcId,
+                "text" : "ec2"
+            }
+            vpc_json.append(ec2ParentDs)
+
+            # Sublist RDS
+            rdsParentDs = {
+                "id" : "rds-" + vpcId,
+                "parent": vpcId,
+                "text" : "rds"
+            }
+            vpc_json.append(rdsParentDs)
+
+            # Sublist S3
+            s3ParentDs = {
+                "id" : "s3-" + vpcId,
+                "parent": vpcId,
+                "text" : "s3"
+            }
+            vpc_json.append(s3ParentDs)
+
+        return vpc_json
+
+    # Get all EC2 Instances
+    def describe_ec2(self):
+        instances = self.ec2.describe_instances()
+        instances = instances['Reservations']
+        ec2_json = []
+        for data1 in instances:
+            instances_details = data1['Instances']
+            for data2 in instances_details:
+                instance_id = data2['InstanceId']
+                ip = data2['PrivateIpAddress']
+                pub_ip = data2["PublicDnsName"]
+                kname = data2["KeyName"]
+                vpc_id = data2['VpcId']
+                ip_address_public = socket.gethostbyname(pub_ip)
+
+                # 
+                tag_ec2 = data2['Tags']
+                for tagss in tag_ec2:
+                    key_ec2 = tagss['Key']
+                    value_ec2 = tagss['Value']
+                    ec2_json.append({"ServiceId or ARN":"{}".format(instance_id),
+                        "VPCID": "{}".format(vpc_id),
+                        "Tag":f"{kname}|{key_ec2}:{value_ec2}",
+                        "Service" : "EC2",
+                        "Public IP": "{}".format(ip_address_public), 
+                            "VPCID":"{}".format(vpc_id),
+                            "Private IP": "{}".format(ip)
+                        })
+        return ec2_json
+
+    # Get all EC2 Instances
+    def describe_ec2_as_tree(self):
+        instances = self.ec2.describe_instances()
+        instances = instances['Reservations']
+        ec2_json = []
+        for data1 in instances:
+            instances_details = data1['Instances']
+            for data2 in instances_details:
+                instance_id = data2['InstanceId']
+                ip = data2['PrivateIpAddress']
+                pub_ip = data2["PublicDnsName"]
+                kname = data2["KeyName"]
+                vpc_id = data2['VpcId']
+
+                subnetid = data2["NetworkInterfaces"][0]["SubnetId"]
+                secgroupname = data2["NetworkInterfaces"][0]["Groups"][0]["GroupName"]
+                cpu_arch=data2["Architecture"]
+                instance_type=data2["InstanceType"]
+                ami_image = data2["ImageId"]
+
+                ip_address_public = socket.gethostbyname(pub_ip)
+                ec2_ds = {
+                    "id" : "ec2-" +instance_id,
+                    "parent": "ec2-" + vpc_id
+                }
+
+                # collect tags 
+                tag_ec2 = data2['Tags']
+                for tagss in tag_ec2:
+                    key_ec2 = tagss['Key']
+                    value_ec2 = tagss['Value']
+                    ec2_ds ["text"] = f"{kname} | {value_ec2} | {instance_id}"
+                ec2_json.append(ec2_ds)
+
+                # Add a detail line per System
+                ec2_detail_ds1 = {
+                    "id" : "ec2-" +instance_id +"-"+ instance_id+"1",
+                    "parent": "ec2-" +instance_id,
+                    "text" : "IP: " +  ip_address_public + " ( " + ip + " ) "
+                }
+                ec2_json.append(ec2_detail_ds1)
+
+                # Add sec group
+                ec2_detail_ds2 = {
+                    "id" : "ec2-" +instance_id +"-"+ instance_id + "2",
+                    "parent":"ec2-" + instance_id,
+                    "text" : "SecurityGroup: " + secgroupname
+                }
+                ec2_json.append(ec2_detail_ds2)
+
+                # Add subnet 
+                ec2_detail_ds3 = {
+                    "id" : "ec2-" +instance_id +"-"+ instance_id + "3",
+                    "parent": "ec2-" +instance_id,
+                    "text" : "SubnetID: " + subnetid
+                }
+                ec2_json.append(ec2_detail_ds3)
+
+                # Add subnet 
+                ec2_detail_ds4 = {
+                    "id" : "ec2-" + instance_id +"-"+ instance_id + "4",
+                    "parent": "ec2-" +instance_id,
+                    "text" : "System: " + instance_type + "(" + cpu_arch + ") | AMI: " + ami_image
+                }
+                ec2_json.append(ec2_detail_ds4)
+
+        return ec2_json
+
+    # Get all RDS Instances
+    def describe_rds(self):
+        resp = self.rds.describe_db_instances()
+        resp = resp['DBInstances']
+        rds_json = []
+        for data in resp:
+            subnet = data['DBSubnetGroup']
+            name = data['DBInstanceIdentifier']
+            vpcid = subnet['VpcId']
+            taglist = data['TagList']
+            arn = data['DBInstanceArn']
+            for tags in taglist:
+                key = tags['Key']
+                value = tags['Value']
+                rds_json.append({'VPCID':f'{vpcid}',
+                    'Service': 'rds', 
+                    'ServiceId or ARN': f'{arn}',
+                    'Tag': f'{name} | {key}:{value}',
+                    'Private IP' : '',
+                    'Public IP' : '' })
+        return rds_json
+        
+    # Get all RDS Instances
+    def describe_rds_as_tree(self):
+        resp = self.rds.describe_db_instances()
+        resp = resp['DBInstances']
+        rds_json = []
+        for data in resp:
+            subnet = data['DBSubnetGroup']
+            instance_id = data['DBInstanceIdentifier']
+            vpcid = subnet['VpcId']
+            taglist = data['TagList']
+            arn = data['DBInstanceArn']
+
+            rds_ds = {
+                    "id" : "rds-"+instance_id,
+                    "parent": "rds-" + vpcid,
+                    "text" : "DBServerName: " + instance_id
+                }
+
+            for tags in taglist:
+                key = tags['Key']
+                value = tags['Value']
+                rds_ds ["text"] = rds_ds ["text"] + "|" + value
+
+            rds_json.append(rds_ds)
+
+            # Add DB Detail
+            rds_detail_ds2 = {
+                "id" : "rds-" + instance_id + "2",
+                "parent": "rds-"+instance_id,
+                "text" : "ARN: " + arn
+            }
+            rds_json.append(rds_detail_ds2)
+
+        return rds_json
+        
+
+
+    # Get all S3 End points 
+    def describe_s3(self):
+        s3_json = []
+        response = self.ec2.describe_vpc_endpoints(
+            Filters=[
+                {
+                    'Name': 'service-name',
+                    'Values': ['com.amazonaws.ap-south-1.s3']
+                }
+            ]
+        )
+        response = response['VpcEndpoints']
+
+        for data in response:
+            vpcId =data['VpcId']
+            tags = data['Tags']
+            for tag in tags:
+                key = tag['Key']
+                value = tag['Value']
+            s3_json.append({"VPCID": f'{vpcId}',"Service":"s3", "Tag": f'Name:{key}|Customer:{value}', 'Public IP' : '', 'Private IP' : '', 'ServiceId or ARN' : ''})
+
+        return s3_json
+
+    # Get all S3 End points 
+    def describe_s3_as_tree(self):
+        s3_json = []
+        response = self.ec2.describe_vpc_endpoints(
+            Filters=[
+                {
+                    'Name': 'service-name',
+                    'Values': ['com.amazonaws.ap-south-1.s3']
+                }
+            ]
+        )
+        response = response['VpcEndpoints']
+
+        for data in response:
+            vpcId =data['VpcId']
+            tags = data['Tags']
+            for tag in tags:
+                key = tag['Key']
+                value = tag['Value']
+        
+        # Qt : Why again - which one is right? 
+        response = self.s3.list_buckets()
+
+        response = response['Buckets']
+        for data in response:
+            name = data['Name']
+            s3_ds = {
+                        "id" : name,
+                        "parent": "s3-" + vpcId,
+                        "text" : f' {name}| {value}'
+                    }
+            s3_json.append (s3_ds)
+        
+        return s3_json 
+
+    def describe(self):
+        vpc_json = self.describe_vpcs()
+        ec2_json = self.describe_ec2()
+        rds_json = self.describe_rds()
+        s3_json = self.describe_s3()
+        return vpc_json, ec2_json, rds_json, s3_json
+
+    def describe_for_tree(self):
+        vpc_json = self.describe_vpcs_as_tree()
+        ec2_json = self.describe_ec2_as_tree()
+        rds_json = self.describe_rds_as_tree()
+        s3_json = self.describe_s3_as_tree()
+        return vpc_json, ec2_json, rds_json, s3_json

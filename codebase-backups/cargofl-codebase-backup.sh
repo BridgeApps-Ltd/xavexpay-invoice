26047c1e2075e964c9aed487b4f4fb05baa1e6ce
KuruppD <dilishkuruppath@gmail.com>
2023-05-02 07:30:13 +0100

    Initial draft

---
+++ codebase-backups/cargofl-codebase-backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bashh
+
+# uplaod to platform-scripts in bridgeapps github codebase
+for i in `cat codebase-list.ini` do
+  echo $i;
+  downloadcodebase;
+  createzip;
+  uploadtos3;
+
+done
+
+function downloadcodebase
+
+function createzip
+
+function uploadtos3

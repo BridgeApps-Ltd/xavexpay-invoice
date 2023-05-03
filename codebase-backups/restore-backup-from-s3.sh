94bb22b9748ce8cdbd5211a0bc0a21f43f08b878
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2023-05-03 15:01:35 +0530

    update variables

--- codebase-backups/restore-backup-from-s3.sh
+++ codebase-backups/restore-backup-from-s3.sh
@@ -5,6 +5,7 @@ S3_BUCKET="$S3_BUCKET"
 REPOSITORY_URL="$REPOSITORY_URL"
 GIT_FOLDER_NAME="$GIT_FOLDER_NAME"
 CURRENT_DATE=$(date +%Y-%m-%d)
+ARCHIVE_PATH="/opt/archive"
 
 # function to upload file to s3 bucket
 restorefroms3() {
@@ -23,10 +24,10 @@ restorefroms3() {
   fi
 
   # Upload the compressed file to S3
-  aws s3 cp "s3://$2/$3/cargofl.zip.gz" "$1"
+  aws s3 cp "s3://$2/$3/$GIT_FOLDER_NAME.zip.gz" "$1"
 
   return 0
 }
 
 # Call the function with the path to the compressed file and S3 bucket name as arguments
-restorefroms3 "/opt/archive/cargofl.zip.gz" "$S3_BUCKET" "$CURRENT_DATE"
+restorefroms3 "$ARCHIVE_PATH/$GIT_FOLDER_NAME.zip.gz" "$S3_BUCKET" "$CURRENT_DATE"

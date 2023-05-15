05c97ff352a19a4c040a02e7594d352fa0bad442
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2023-05-15 11:00:44 +0530

    created updatedns.sh

--- godaddy/updatedns.sh
+++ godaddy/updatedns.sh
@@ -7,11 +7,16 @@ TYPE="$TYPE"
 KEY="$KEY"
 SECRET="$SECRET"
 
-WanIP=`curl -s "https://api.ipify.org"`
-echo $WanIP
+SERVER_IP=`curl -s "https://api.ipify.org"`
 
-GDIP=`curl -s -X GET -H "Authorization: sso-key ${KEY}:${SECRET}" "https://api.godaddy.com/v1/domains/${DOMAIN}/records/${TYPE}/${HOST}" | cut -d'[' -f 2 | cut -d']' -f 1 | jq -r '.data'`
+GODADDY_IP=`curl -s -X GET -H "Authorization: sso-key ${KEY}:${SECRET}" "https://api.godaddy.com/v1/domains/${DOMAIN}/records/${TYPE}/${HOST}" | cut -d'[' -f 2 | cut -d']' -f 1 | jq -r '.data'`
 
-if [ "$WanIP" != "$GDIP" -a "$WanIP" != "" ]; then
-        curl -s -X PUT "https://api.godaddy.com/v1/domains/${DOMAIN}/records/${TYPE}/${HOST}" -H "Authorization: sso-key ${KEY}:${SECRET}" -H "Content-Type: application/json" -d "[{\"data\": \"${WanIP}\"}]"
+if [ "$SERVER_IP" != "$GODADDY_IP" -a "$SERVER_IP" != "" ]; then
+echo "Updating IP Address"
+        curl -s -X PUT "https://api.godaddy.com/v1/domains/${DOMAIN}/records/${TYPE}/${HOST}" -H "Authorization: sso-key ${KEY}:${SECRET}" -H "Content-Type: application/json" -d "[{\"data\": \"${SERVER_IP}\"}]"
+echo "Updated"
+fi
+
+if [ "$SERVER_IP" = "$GODADDY_IP" ]; then
+echo "Server and Godaddy IP's are equal, no update is required"
 fi

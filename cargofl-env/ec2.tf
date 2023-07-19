b22c2e3c6e4587433eaed603777c441f5cb84e42
Ananthuaxd <117087690+Ananthuaxd@users.noreply.github.com>
2023-07-19 18:29:49 +0530

    Create ec2.tf

---
+++ cargofl-env/ec2.tf
@@ -0,0 +1,108 @@
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region     = "ap-south-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "vpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "InternetGateway"
+  }
+}
+
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "RouteTable"
+  }
+}
+
+resource "aws_route_table_association" "my_rt_assoc" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "MySecurityGroup"
+  description = "My Security Group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["52.66.175.137/32"]
+  }
+
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SecurityGroup"
+  }
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-07eaf27c7c4a884cf"  # Specify the desired AMI ID
+  instance_type = "t2.micro"      # Specify the desired instance type
+
+  subnet_id      = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  associate_public_ip_address = true
+  key_name       = "PRODUCTION1"
+  tags = {
+    Name = "test"
+  }
+}

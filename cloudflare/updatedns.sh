53d9ce2bccc98784a9d944b348f97ce7043a30df
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2024-02-01 23:59:34 +0530

    created script to update all the given records into cloudflare dns

--- cloudflare/updatedns.sh
+++ cloudflare/updatedns.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 # Source configuration variables
-source config.ini
-
-# Assign variables from config
-zone=$zone
-dnsrecord=$dnsrecord
-cloudflare_auth_email=$cloudflare_auth_email
-cloudflare_auth_key=$cloudflare_auth_key
+source .env
 
 # Function to display a formatted message
 show_message() {
@@ -35,34 +29,36 @@ addressip=$(curl -s -X GET https://checkip.amazonaws.com)
 
 echo "Current IP is $addressip"
 
-# Check if the DNS record is already set to the current IP address
-if host $dnsrecord 1.1.1.1 | grep "has address" | grep "$addressip"; then
-  echo "$dnsrecord is currently set to $addressip; no changes needed"
-  exit
-fi
+# Loop through each DNS record
+for dnsrecord in "${dnsrecords[@]}"; do
+    echo "Checking DNS record $dnsrecord"
+    # Check if the DNS record is already set to the current IP address
+    if host "$dnsrecord" 1.1.1.1 | grep "has address" | grep "$addressip"; then
+      echo "$dnsrecord is currently set to $addressip; no changes needed"
+    else
+      show_message "Updating DNS record $dnsrecord"
+      
+      # Get the zone ID for the requested zone
+      zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
+        -H "X-Auth-Email: $cloudflare_auth_email" \
+        -H "Authorization: Bearer $cloudflare_auth_key" \
+        -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
 
-# If here, the DNS record needs updating
-show_message "Updating DNS record"
+      echo "Zoneid for $zone is $zoneid"
 
-# Get the zone ID for the requested zone
-zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
-  -H "X-Auth-Email: $cloudflare_auth_email" \
-  -H "Authorization: Bearer $cloudflare_auth_key" \
-  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+      # Get the DNS record ID
+      dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
+        -H "X-Auth-Email: $cloudflare_auth_email" \
+        -H "Authorization: Bearer $cloudflare_auth_key" \
+        -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
 
-echo "Zoneid for $zone is $zoneid"
+      echo "DNSrecordid for $dnsrecord is $dnsrecordid"
 
-# Get the DNS record ID
-dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
-  -H "X-Auth-Email: $cloudflare_auth_email" \
-  -H "Authorization: Bearer $cloudflare_auth_key" \
-  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
-
-echo "DNSrecordid for $dnsrecord is $dnsrecordid"
-
-# Update the DNS record
-curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
-  -H "X-Auth-Email: $cloudflare_auth_email" \
-  -H "Authorization: Bearer $cloudflare_auth_key" \
-  -H "Content-Type: application/json" \
-  --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$addressip\",\"ttl\":1,\"proxied\":true}" | jq
+      # Update the DNS record
+      curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
+        -H "X-Auth-Email: $cloudflare_auth_email" \
+        -H "Authorization: Bearer $cloudflare_auth_key" \
+        -H "Content-Type: application/json" \
+        --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$addressip\",\"ttl\":1,\"proxied\":true}" | jq
+    fi
+done

d7819ce02685c35a7948b3271a01c32d85c4372c
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2024-06-11 17:41:32 +0530

    Update db_s3_backup.sh

--- scripts/Linux/db_s3_backup.sh
+++ scripts/Linux/db_s3_backup.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-
-#########################################################################
-#Backup script for CARGOFL, that take sql backup and uploads to S3 Bucket.
-#########################################################################
+##---------- Author : Mubinahmed Shaikh -----------------------------------------------------##
+##---------- Purpose : Take cargofl db sql backup and uploads to S3 Bucket ------------------##
+##---------- Tested on : Ubuntu 22/20/18 ----------------------------------------------------##
+##---------- Updated version : v2.0 (Updated on 11th JUNE 2024) -----------------------------##
+##-----NOTE: This script does't require root privileges, one could run the script directly --##
+##----------- "sudo /bin/bash <ScriptName>" -------------------------------------------------##
+##---- Requirements: Install and configure awscli and mysql-client --------------------------##
 
 DATE=$(date +%Y-%m-%d-%H)
-RDS_HOST="sample.cu0uoeslvlzn.ap-south-1.rds.amazonaws.com"
+RDS_HOST="sample.cupuoeslvlzn.ap-south-1.rds.amazonaws.com"
 RDS_PORT="3306"
 RDS_USER="innoctive"
 RDS_PASSWORD="password"
 DB_NAME=cargofl
-S3_BUCKET="cargofl-decathlon-prod-dbbackups"
-DB_IDENTIFIER="mysqldump_decathlon-prod"
+S3_BUCKET="cargofl-example-prod-dbbackups"
+DB_IDENTIFIER="mysqldump_example-prod"
 MINSANDSECS=$(date +%M-%S)
 
 echo "Taking backup"
-mysqldump -h $RDS_HOST -u $RDS_USER -p'NAp8iW8RZavA5tTDi1Fm' $DB_NAME > $DB_IDENTIFIER_$DATE.sql
+mysqldump -h $RDS_HOST -u $RDS_USER -p'samplepassword' $DB_NAME > $DB_IDENTIFIER_$DATE.sql
 
 # Upload the mysqldump to the S3 bucket
 echo "uploading to S3 Bucket"
@@ -24,3 +27,7 @@ aws s3 cp $DB_IDENTIFIER_$DATE.sql s3://$S3_BUCKET/$DB_IDENTIFIER-$DATE-$MINSAND
 # Remove the local copy of the mysqldump
 echo "deleting  unwanted files"
 rm $DB_IDENTIFIER_$DATE.sql
+
+echo -e "\n\t\t %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" 
+echo -e "\t\t   <>----------------------<> End OF THE SCRIPT  <>-------------------<>"
+echo -e "\t\t %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" 

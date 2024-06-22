73bb6174e70d9bd42b8249d2b53d87d40c598ae5
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2024-06-22 11:05:15 +0530

    Updated new codebases

--- scripts/Linux/ServiceHealthCheck.sh
+++ scripts/Linux/ServiceHealthCheck.sh
@@ -1,51 +1,28 @@
 #!/bin/bash
-red=`tput setaf 1`
-green=`tput setaf 2`
-yellow=`tput setaf 3`
-white=`tput setaf 7`
-#To check tomcat is running or not
- echo ' '
- TOMCAT=`service tomcat status | grep -o "running"`
- if [[ $TOMCAT == "running" ]]
- then
- echo "${white}1. Tomcat for UATX1 is ${green}active (running)"
- echo "${white}Health status = ${green}[ok]"
- else
- echo "${white}1. Tomcat for UATX1 is not running"
- echo "Health status = ${red}[failure]"
- echo "${white}Restarting tomcat"
-       service tomcat start
- echo "Tomcat restarted"
- fi
- echo "${yellow}-------------------------------------"
-#To check Nginx is running or not
- NGINX=`service nginx status | grep -o "running"`
- if [[ $NGINX == "running" ]]
- then
- echo "${white}2. Nginx is running"
- echo "Health status = ${green}[ok]"
- else
- echo "${white}2. Nginx is not running"
- echo "Health status = ${red}[failure]"
- echo "${white}Restarting nginx webserver"
-       service nginx start
- echo "nginx restarted"
 
- fi
- echo "${yellow}-------------------------------------"
+# Define color codes
+RED='\033[0;31m'  # Red color
+GREEN='\033[0;32m'  # Green color
+NC='\033[0m'     # No color
 
-#To check PHP_FPM running or not
- PHP_FPM=`service php7.2-fpm status | grep -o "running"`
- if [[ $PHP_FPM == "running" ]]
- then
- echo "${white}3. PHP-FPM is running"
- echo "Health status = ${green}[ok]"
- else
- echo "${white}3. PHP-FPM is not running"
- echo "Health status = ${red}[failure]"
- echo "${white}Restarting php7.2-fpm"
-       service php7.2-fpm start
- echo "php7.2-fpm restarted"
- fi
- echo "${yellow}-------------------------------------"
+# Function to check and print the status
+check_service() {
+  local service_name="$1"
+  local status="$(systemctl is-active --quiet "$service_name" && echo -e "${GREEN}[ACTIVE]${NC}" || echo -e "${RED}[INACTIVE]${NC}")"
+  printf "%-3s %-20s %-15s\n" "$count." "$service_name:" "$status"
+  ((count++)) # Increment counter
+}
 
+# Print table headers
+printf "%-3s %-20s %-15s\n" "No." "Service Name" "Status"
+echo "------------------------------------------"
+
+# Check the status of each service
+count=1
+check_service "npm-godrej"
+check_service "npm-tk"
+check_service "npm-western"
+check_service "npm-sintex"
+check_service "npm-dc"
+check_service "npm-bmx"
+check_service "npm-nourishco"

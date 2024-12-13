deac5ad9063002e834caad100576a89b0792d963
mubininnoctive <87492644+mubininnoctive@users.noreply.github.com>
2024-12-13 17:22:49 +0530

    Add 'help' command to techchallenge script for email support

--- tech_skills_challenge/techchallenge.sh
+++ tech_skills_challenge/techchallenge.sh
@@ -4,9 +4,14 @@
 TIME_FILE="/tmp/techchallenge_start_time"
 LOG_FILE="/var/cache/somewhere/logger/backpacks.log"
 
+# Email details
+SOURCE="no-reply@cargofl.com"
+RECIPIENTS="mubin@cargofl.com"
+AWS_REGION="ap-south-1"
+
 # Check if the user provided an argument
 if [ $# -eq 0 ]; then
-  echo "Usage: techchallenge [start|finish]"
+  echo "Usage: techchallenge [start|finish|help]"
   exit 1
 fi
 
@@ -16,7 +21,17 @@ log_message() {
   echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> "$LOG_FILE"
 }
 
-# Handle the "start" and "finish" commands
+# Function to send email
+send_help_email() {
+     aws ses send-email \
+      --from "$SOURCE" \
+      --destination "ToAddresses=${RECIPIENTS}" \
+      --message "Subject={Data=CargoFL TechChallenge Help Request!},Body={Text={Data=The User $USER has requested help with the TechChallenge. Please assist him or her.}}" \
+      --region "$AWS_REGION"
+  log_message "Help email sent to CargoFL Team. The Team will support you as soon as possible"
+}
+
+# Handle the commands
 case $1 in
   start)
     # Record the start time
@@ -24,7 +39,7 @@ case $1 in
     echo "TechChallenge has started!"
     log_message "TechChallenge started."
     ;;
-  
+
   finish)
     # Check if the start time exists
     if [ -f "$TIME_FILE" ]; then
@@ -34,16 +49,16 @@ case $1 in
       end_time=$(date +%s)
       # Calculate the elapsed time in seconds
       elapsed_time=$((end_time - start_time))
-      
+
       # Format the elapsed time as hours, minutes, and seconds
       hours=$((elapsed_time / 3600))
       minutes=$(((elapsed_time % 3600) / 60))
       seconds=$((elapsed_time % 60))
-      
+
       echo "TechChallenge has finished!"
       echo "Time elapsed: ${hours}h ${minutes}m ${seconds}s"
       log_message "TechChallenge finished. Time elapsed: ${hours}h ${minutes}m ${seconds}s"
-      
+
       # Remove the time file
       rm "$TIME_FILE"
     else
@@ -52,9 +67,14 @@ case $1 in
       exit 1
     fi
     ;;
-  
+
+  help)
+    echo "Help request sent to $HELP_EMAIL."
+    send_help_email
+    ;;
+
   *)
-    echo "Invalid command. Use 'start' or 'finish'."
+    echo "Invalid command. Use 'start', 'finish', or 'help'."
     log_message "Invalid command used: $1"
     exit 1
     ;;
